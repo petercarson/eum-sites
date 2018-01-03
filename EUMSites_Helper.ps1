5507450c6f76ed9a414a1fee0046489ce59632fa
petercarson <peter@envisionit.com>
2018-01-02 23:55:28 -0500

    Added CreateSites script and updated supporting files

--- EUMSites_Helper.ps1
+++ EUMSites_Helper.ps1
@@ -28,6 +28,7 @@
     [string]$Global:WebAppURL = $environment.webApp.url
     [string]$Global:TenantAdminURL = $environment.webApp.adminSiteURL
     [string]$Global:SitesListSiteURL = "$($WebAppURL)$($environment.webApp.sitesListSiteCollectionPath)"
+    [string]$Global:SiteListName = $config.settings.common.siteLists.siteListName
     [string]$Global:ManagedCredentials = $environment.webApp.managedCredentials
 
     [string]$Global:EUMClientID = $environment.EUM.clientID
@@ -368,5 +369,49 @@ function ImportContentTypes
         }
     } #foreach ContentType
 
+}
+
+function CheckIfSiteCollection()
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)][string] $siteURL
+    )
+    [bool] $isSiteCollection = $false
+    foreach($managedPath in $managedPaths)
+    {
+        
+        [string]$relativeURL = $siteURL.Replace($WebAppURL, "").ToLower().Trim()
+
+        if ($relativeURL -eq '/')
+        {
+            $isSiteCollection = $true
+        }
+        elseif ($relativeURL.StartsWith(($managedPath.ToLower())))
+        {
+            [string]$relativeURLUpdated = $relativeURL.Replace($managedPath.ToLower(), "").Trim('/')
+            [int]$charCount = ($relativeURLUpdated.ToCharArray() | Where-Object {$_ -eq '/'} | Measure-Object).Count
+            
+            $isSiteCollection = $charCount -eq 0
+        }
+    }
+
+    return $isSiteCollection
+}
+
+function GetBreadcrumbHTML()
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)][string] $siteRelativeURL,
+        [Parameter(Mandatory=$true)][string] $siteTitle,
+        [Parameter(Mandatory=$false)][string] $parentBreadcrumbHTML
+    )
+    [string]$breadcrumbHTML = "<a href=`"$($siteRelativeURL)`">$($siteTitle)</a>"
+	if ($parentBreadcrumbHTML)
+	{
+		$breadcrumbHTML = $parentBreadcrumbHTML + ' &gt; ' + $breadcrumbHTML
+	}
+    return $breadcrumbHTML
 }
 
