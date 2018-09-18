b6fb72ab7618b386742451006e9c11c386fcd2f5
petercarson <peter@envisionit.com>
2018-09-18 02:34:20 -0400

    Added Masthead support

--- CreateSite/run.ps1
+++ CreateSite/run.ps1
@@ -18,6 +18,8 @@ if (-not $DistributionFolder)
 . $DistributionFolder\EUMSites_Helper.ps1
 LoadEnvironmentSettings
 
+. $DistributionFolder\masthead.ps1 -username $SPCredentials.UserName -password $SPCredentials.Password -MastheadInstallerSite "https://envisionit.sharepoint.com/sites/home"
+
 if ($listItemID)
 {
     # Get the specific Site Collection List item in master site for the site that needs to be created
@@ -205,12 +207,19 @@ if ($pendingSiteCollections.Count -gt 0)
 
         if ($siteCreated)
         {
+            Helper-Connect-PnPOnline -Url $siteURL
+            
+            # Set the site collection admins
+            Add-PnPSiteCollectionAdmin -Owners "pcarson@envisionit.com"
+
+            Start-Sleep -Seconds 15
+
             if ($pnpSiteTemplate)
             {
-                Helper-Connect-PnPOnline -Url $siteURL
                 Apply-PnPProvisioningTemplate -Path $pnpSiteTemplate -ExcludeHandlers Publishing, ComposedLook, Navigation
-                Disconnect-PnPOnline
             }
+
+            Disconnect-PnPOnline
             
             # Reconnect to the master site and update the site collection list
             Helper-Connect-PnPOnline -Url $SitesListSiteURL
@@ -231,6 +240,9 @@ if ($pendingSiteCollections.Count -gt 0)
 
             # Set the site created date, breadcrumb, and site URL
             [Microsoft.SharePoint.Client.ListItem]$spListItem = Set-PnPListItem -List $SiteListName -Identity $pendingSite.Id -Values @{ "EUMSiteCreated" = [System.DateTime]::Now; "EUMBreadcrumbHTML" = $breadcrumbHTML; "EUMSiteURL" = $siteRelativeURL }
+
+            # Install Masthead on the site
+            Install-To-Site $siteURL
         }
 
         # Reconnect to the master site for the next iteration
