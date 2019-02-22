c986d05f7512c4e877f4765c2d043f5aaf7966ea
Peter Carson <pcarson@envisionit.com>
2019-02-22 09:44:50 -0500

    Added PowerShell post-PnP steps to script

--- ApplySiteTemplate/ApplySiteTemplate.ps1
+++ ApplySiteTemplate/ApplySiteTemplate.ps1
@@ -14,8 +14,27 @@ LoadEnvironmentSettings
 [string]$SiteURL = Read-Host "Enter the URL of the site to apply the template to"
 
 Helper-Connect-PnPOnline -Url $siteURL
+
 Set-PnPTraceLog -On -Level Debug
 $pnpSiteTemplate = $DistributionFolder + "\SiteTemplates\Client-Template-Template.xml"
 Apply-PnPProvisioningTemplate -Path $pnpSiteTemplate
+
+Add-PnPFolder -Name "Quotes" -Folder "/Shared Documents"
+Add-PnPFolder -Name "Signed Quotes" -Folder "/Shared Documents/Quotes"
+Add-PnPFolder -Name "Invoices" -Folder "/Shared Documents"
+
+Add-PnPFolder -Name "Business Development" -Folder "/Private Documents"
+Add-PnPFolder -Name "Confidential" -Folder "/Private Documents"
+Add-PnPFolder -Name "Quotes" -Folder "/Private Documents"
+
+Remove-PnPContentTypeFromList -List "Shared Documents" -ContentType "Document"
+Remove-PnPContentTypeFromList -List "Private Documents" -ContentType "Document"
+
+$spWeb = Get-PnPWeb
+$Title = $spWeb.Title
+Remove-PnPGroup -Identity "$Title Members" -Force
+Remove-PnPGroup -Identity "$Title Owners" -Force
+Remove-PnPGroup -Identity "$Title Visitors" -Force
+
 Disconnect-PnPOnline
             
